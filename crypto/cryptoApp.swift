a74c9cbc2b4af0b14151ef447b7a057f2ec8e617
vzverev <vzverev@freetechs.org>
2023-09-04 10:26:38 +0400

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by user on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
